c182720e81e678f01a8eafb1fbfd0cd83b280bc9
Yaroslav <ya.galyuk@gmail.com>
2021-07-28 16:35:27 +0300

    React-test-renderer
    Add test for ProfileStatus component:
    - check <span> appear
    - check correct status
    - check <input> appear
    - check activate editMode

---
+++ client/src/AppContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/redux-store";
+import App from "./App";
+
+const AppContainer = () => {
+    return (
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    );
+};
+
+export default AppContainer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import store from "./redux/redux-store";
+import AppContainer from "./AppContainer";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
+        <AppContainer/>
     </React.StrictMode>,
     document.getElementById('root')
 );
